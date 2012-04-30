6bf1e55facf573f6ae2c47406032a8c38f18a436
Anh Phan <anh.phan@inbox.com>
2012-04-30 15:03:28 +0200

    Version 1.4.7.5

---
+++ index.php
@@ -0,0 +1,77 @@
+<html lang="en">
+<!-- 
+Smart developers always View Source. 
+
+This application was built using Adobe Flex, an open source framework
+for building rich Internet applications that get delivered via the
+Flash Player or to desktops via Adobe AIR. 
+
+Learn more about Flex at http://flex.org 
+// -->
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+
+    <link rel="stylesheet" type="text/css" href="http://play.dackyvn.net/history/history.css" />
+    <link rel="stylesheet" type="text/css" href="http://play.dackyvn.net/history/style.css" />
+	<link rel="stylesheet" type="text/css" href="http://play.dackyvn.net/config/init/css/live_notify_css.css"/>    
+
+	<script src="http://play.dackyvn.net/AC_OETags.js" language="javascript"></script>
+    <script src="http://play.dackyvn.net/history/history.js" language="javascript"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" language="javascript"></script>
+    <script src="http://play.dackyvn.net/config/init/initMsg.js" language="javascript"></script>
+
+    <script type="text/javascript" src="http://play.dackyvn.net/config/init/js/live_notify.js"></script>
+      
+    
+    <style>
+    body { margin: 0px; background-color:#000000}
+    .td01{ color:#fff; padding:10px 0px 10px 200px; background:url(http://download.51fswd.com/FlashPlay/img/01.png) no-repeat top left;}
+    .td01 a{color:#FF0000;}
+    </style>
+</head>
+
+<body background="img/di.jpg">   
+	
+	<table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+	    <tr align="center" valign="middle">
+	        <td></td>
+		<td></td>
+		<td></td>
+	    </tr>
+	    <tr align="center" valign="middle">
+	        <td></td>
+	        <td align="center" valign="middle">
+	            <table width="1032" border="0" cellpadding="0" cellspacing="0">
+	                <tr>
+	                    <td width="16" height="7" align="left" valign="top"><img src="img/jiao01.gif" width="16" height="7"></td>
+	                    <td align="center" valign="middle"><img src="img/bian.jpg" width="1008" height="4"></td>
+	                    <td width="10" align="left" valign="top"><img src="img/jiao01.gif" width="16" height="7"></td>
+	                </tr>
+	                <tr >
+	                    <td width="18" align="left" valign="top" background="img/zuobian.jpg" style="background-repeat:repeat-y">&nbsp;</td>
+	                    <td  align="center" valign="middle">
+	                        <div id="gameplayer">&nbsp;</div>
+	                    </td>
+	                    <td width="10" align="center" valign="top" background="img/zuobian.jpg">&nbsp;</td>
+	                </tr>
+	                <tr>
+	                    <td height="7" align="left" valign="top"><img src="img/jiao02.gif" width="16" height="7"></td>
+	                    <td align="center" valign="middle"><img src="img/bian.jpg" width="1008" height="4"></td>
+	                    <td align="center" valign="top"><img src="img/jiao02.gif" width="16" height="7"></td>
+	                </tr>
+	            </table>
+	        </td>
+	        <td></td>
+	    </tr>
+	    <tr align="center" valign="middle">
+	        <td></td>
+		<td></td>
+		<td></td>
+	    </tr>
+	</table>
+
+   
+</body>
+</html>
